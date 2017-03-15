623fb1838ebfcf014b8dd6990033be1c85d13149
elkade <dragan1993@gmail.com>
2017-03-15 10:02:32 +0100

    edycja pól

--- app/src/main/java/com/example/lukas/homework/EditFragment.java
+++ app/src/main/java/com/example/lukas/homework/EditFragment.java
@@ -9,23 +9,32 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 
 
 public class EditFragment extends Fragment {
+
+    private View view;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater,
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_edit, container, false);
+        view = inflater.inflate(R.layout.fragment_edit, container, false);
         Button loginButton = (Button) view.findViewById(R.id.apply_button);
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Context context = getContext();
                 ShoppingItemsRepository repo = new ShoppingItemsRepository(context);
-                repo.insertItem(new ShoppingItem("ABC", "DEF"));
+
+                EditText editTitle = (EditText) view.findViewById(R.id.edit_title);
+                EditText editDescription = (EditText) view.findViewById(R.id.edit_description);
+
+                ShoppingItem shoppingItem = new ShoppingItem(editTitle.getText().toString(), editDescription.getText().toString());
+                repo.insertItem(shoppingItem);
+
                 startActivity(new Intent(getActivity(), MainActivity.class));
             }
         });
